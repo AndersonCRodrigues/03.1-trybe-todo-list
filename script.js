a3ab770d7b414ad6b112a8495a820f48d41f89ce
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-01 15:29:44 -0300

    adiciona requisito 12

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function criaElemento(elemento, texto, atributo, valor) {
 function addTarefa() {
   const input = document.querySelector('#texto-tarefa');
   if (input.value !== '') {
-    const tarefa = criaElemento('li', '', 'class', 'item-tasks');
+    const tarefa = criaElemento('li', '', 'id', '');
     const lista = document.querySelector('#lista-tarefas');
     tarefa.innerText = input.value;
     lista.appendChild(tarefa);
@@ -17,18 +17,16 @@ function addTarefa() {
 }
 
 function removeBG() {
-  listTask = document.querySelectorAll('.item-tasks');
+  listTask = document.querySelectorAll('li');
   for (item of listTask) item.style.backgroundColor = '';
 }
 
 function selectItem(event) {
   const selected = event.target;
-  console.log(selected);
   if (selected.style.backgroundColor === 'grey') selected.style.backgroundColor = '';
   else {
     removeBG();
     selected.style.backgroundColor = 'grey';
-    selected.classList.add('selected');
   }
 }
 
@@ -55,6 +53,34 @@ function removeFinalizado() {
   }
 }
 
+function salvaTarefas() {
+  listTask = document.querySelectorAll('li');
+  const array = [];
+  for (const info of listTask) {
+    const value = {
+      class: '',
+      text: '',
+    };
+    value.class = info.className;
+    value.text = info.innerText;
+    array.push(value);
+  }
+  localStorage.setItem('tasks', JSON.stringify(array));
+}
+
+function loadTasks() {
+  listTask = document.querySelector('#lista-tarefas');
+  const arrayTasks = JSON.parse(localStorage.getItem('tasks'));
+  listTask = document.querySelector('#lista-tarefas');
+  if (localStorage.tasks) {
+    for (const value of arrayTasks) {
+      console.log(value.text);
+    listTask.appendChild(criaElemento('li', `${value.text}`, 'class', `${value.class}`));
+    console.log(value.class);
+    }
+  }
+}
+
 const body = document.getElementsByTagName('body')[0];
 body.appendChild(criaElemento('header', 'Minha Lista de Tarefas', 'class', 'header'));
 const textP = 'Clique duas vezes em um item para marcá-lo como completo';
@@ -64,6 +90,7 @@ body.appendChild(criaElemento('ol', '', 'id', 'lista-tarefas'));
 body.appendChild(criaElemento('button', 'criar-tarefa', 'id', 'criar-tarefa'));
 body.appendChild(criaElemento('button', 'Apaga Tudo', 'id', 'apaga-tudo'));
 body.appendChild(criaElemento('button', 'Remove Finalizado', 'id', 'remover-finalizados'));
+body.appendChild(criaElemento('button', 'Salva Tarefas', 'id', 'salvar-tarefas'));
 
 btnTarefas = document.querySelector('#criar-tarefa');
 btnTarefas.addEventListener('click', addTarefa);
@@ -79,3 +106,8 @@ btnApagaTudo.addEventListener('click', limpaLista);
 
 btnRemoveFinalizado = document.querySelector('#remover-finalizados');
 btnRemoveFinalizado.addEventListener('click', removeFinalizado);
+
+btnSalvatarefas = document.querySelector('#salvar-tarefas');
+btnSalvatarefas.addEventListener('click', salvaTarefas);
+
+loadTasks();
