91110edf3c180ccfa481382c44c372c1a30f0c9e
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-01 11:31:05 -0300

    adiciona requisito 02

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ function criaElemento(elemento, texto, atributo, valor) {
 
 const body = document.getElementsByTagName('body')[0];
 body.appendChild(criaElemento('header', 'Minha Lista de Tarefas', 'class', 'header'));
+body.appendChild(criaElemento('p', 'Clique duas vezes em um item para marcá-lo como completo', 'id', 'funcionamento'));
